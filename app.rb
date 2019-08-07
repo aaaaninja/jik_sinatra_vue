40a05ede5fe342d7ba0a262e248df6994f0bd48e
aaaaninja <keshigomu71505@gmail.com>
2019-08-07 19:46:21 +0900

    まぁいろいろと初期の設定が終わったかな?

---
+++ app.rb
@@ -0,0 +1,15 @@
+require "sinatra/base"
+require "sinatra/reloader"
+
+class MyApp < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+################################################################################
+
+  get "/" do
+    "hello, world!abababa"
+  end
+
+  run! if app_file == $0 # ファイルを読み込むとサーバを実行する
+end
