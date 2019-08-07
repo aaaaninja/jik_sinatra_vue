54792833175e8fb0e64eabc246606f8249a9f645
aaaaninja <keshigomu71505@gmail.com>
2019-08-07 20:33:46 +0900

    :bug: rspecが担えるテストの範疇を超えたので削除

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -6,8 +6,4 @@ describe "My Sinatra Application" do
   it "should allow accessing the home page" do
     expect(last_response).to be_ok
   end
-
-  it "should discribe message" do
-    expect(last_response.body).to include('hogehogeeeeeee')
-  end
 end
